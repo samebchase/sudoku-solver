24b8a682c217f24fd39172bcc176ebc4e062f214
Samuel Chase <samebchase@gmail.com>
2011-08-21 20:06:27 +0530

    Able to read from file and store into grid

---
+++ sudoku-solver.lisp
@@ -0,0 +1,20 @@
+(setq grid (make-array '(9 9)))
+
+(let ((in (open "puzzle.txt")))
+  (when in
+    (loop for line = (read-line in nil) while line
+       for i upto 8
+       do
+	 (loop for char across line for j upto 8
+	    do
+	      (setf (aref grid i j) (parse-integer (coerce (list char) 'string)))))
+    (close in)))
+
+(print grid)
+
+
+
+
+
+
+    
