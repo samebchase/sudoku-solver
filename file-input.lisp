68ececba69fe7ff87be60c2dab0a626bfb365431
Samuel Chase <samebchase@gmail.com>
2013-08-17 00:01:47 +0530

    Improve readability

--- file-input.lisp
+++ file-input.lisp
@@ -1,14 +1,20 @@
 (in-package :sudoku-solver)
 
-(defun read-lines (path-str)
-  (with-open-file (stream path-str)
-    (loop for line = (read-line stream nil)
-       while line collect line)))
-
 (defun first-char-alphap (string)
   (alpha-char-p (aref string 0)))
 
+(defun read-lines (path-str)
+  (with-open-file (stream path-str)
+    (loop
+       for line = (read-line stream nil)
+       while line
+       collect line)))
+
 (defun row-strings-to-puzzle (puzzle list)
-  (loop for i to 8 for string in list
-     do (loop for j to 8 for char across string
-           do (setf (aref (grid puzzle) i j) (digit-char-p char)))))
+  (loop for i to 8
+     for string in list do
+       (loop
+          for j to 8
+          for char across string do
+            (setf (aref (grid puzzle) i j)
+                  (digit-char-p char)))))
--- sudoku-solver.lisp
+++ sudoku-solver.lisp
@@ -6,10 +6,11 @@
   puzzle)
 
 (defun puzzle-list () 
-  (loop for string-list
-     in (group-into 9 (remove-if #'first-char-alphap (read-lines *euler-puzzles-path*)))
-     for puzzle = (make-instance 'sudoku-puzzle)
-     do (row-strings-to-puzzle puzzle string-list)
+  (loop
+     for string-list in
+       (group-into 9 (remove-if #'first-char-alphap (read-lines *euler-puzzles-path*)))
+     for puzzle = (make-instance 'sudoku-puzzle) do
+       (row-strings-to-puzzle puzzle string-list)
      collect puzzle))
 
 (defparameter *puzzles* (puzzle-list))
